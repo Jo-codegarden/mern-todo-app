ace7afab3a7b7050db928f282b4d442b1d96c89b
Jo <test@gmail.com>
2023-02-14 13:29:41 +1300

    fixing delete null

--- src/routes/deleteTodoRoute.js
+++ src/routes/deleteTodoRoute.js
@@ -3,6 +3,9 @@ const TodoModel = require('../models/TodoModel');
 module.exports = async (req, res) => {
     const {id} = req.params;
     const todo = await TodoModel.findById(id);
+    if (!todo) {
+        return res.status(404).json({error: "Todo item not found"});
+    }
     await todo.remove();
     res.status(204).json(todo);
-}
+};
